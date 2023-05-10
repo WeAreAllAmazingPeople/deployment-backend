aa2cdd2d4192999ba434aa2008b31d7dc8da4c5a
WeAreAllAmazingPeople <48586979+WeAreAllAmazingPeople@users.noreply.github.com>
2023-05-10 13:30:35 +0200

    Update CORS

--- src/main/java/com/switchfully/secretmanagerbackend/WebConfiguration.java
+++ src/main/java/com/switchfully/secretmanagerbackend/WebConfiguration.java
@@ -10,7 +10,7 @@ public class WebConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://main--phenomenal-cascaron-82ffc1.netlify.app")
+                .allowedOrigins("https://endearing-torte-dbc5b7.netlify.app")
                 .allowedMethods("*");
     }
 }
